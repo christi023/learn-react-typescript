167a3210c8b19a302334dfc65121319d4ef9e71f
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-09-25 20:53:58 +0200

    imported components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import './App.css';
+import Greet from './Components/Greet';
+import Person from './Components/Person';
+import PersonList from './Components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Jim',
+    last: 'Olsson'
+  }
+
+  // array as props
+  const nameList = [
+    {
+      id: 1,
+      first: 'Lucas',
+      last: 'Steele',
+    },
+    {id:2,
+      first: 'Lily',
+      last: 'Mills',
+    },
+    {id:3,
+      first: 'Ally',
+      last: 'Jones',
+    },
+  ];
   return (
     <div className="App">
-     <h1>Hello App</h1>
+      <Greet name='Lily' messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList}/>
     </div>
   );
 }
 
 export default App;
+
