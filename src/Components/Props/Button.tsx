98b33fd6c26183691c8f6d26aa984774605d8bdd
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-09-27 17:02:29 +0200

    passing button as a prop with id

---
+++ src/Components/Props/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// events and button with id parameter
+type ButtonProps = {
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>, id:number) => void // do not return anything
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button onClick={(event) => props.handleClick(event, 1)}>
+      Click
+    </button>
+  );
+};
+
+export default Button;
