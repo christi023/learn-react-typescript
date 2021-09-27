dc3294bb42b6c080b14bc97f3431d24948136f98
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-09-27 17:04:57 +0200

    string, number and boolean as props

--- src/Components/Greet.tsx
+++ src/Components/Greet.tsx
@@ -1,14 +1,15 @@
 // string and booleans props
 type GreetProps = {
   name: string
-  messageCount: number
+  messageCount?: number // here ? is optional props
   isLoggedIn: boolean
 }
 
 export default function Greet(props: GreetProps) {
+  const { messageCount = 0} = props
   return (
     <div>
-      <h2>{props.isLoggedIn ? `Welcome ${props.name}! You have ${props.messageCount} messages` : 'Welcome Guest'}</h2>
+      <h2>{props.isLoggedIn ? `Welcome ${props.name}! You have ${messageCount} messages` : 'Welcome Guest'}</h2>
     </div>
   )
 }
