8c868a299da99387ac36e72eec8f205f8a7bce80
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-09-28 14:30:41 +0200

    typescript Generics

---
+++ src/Components/Generics/List.tsx
@@ -0,0 +1,23 @@
+// type props
+type ListProps<T> = {
+  items: T[];
+  onClick: (value: T) => void;
+};
+
+// accepts array of items and handle a click event on each of these items
+// T means we can pass in an array of any type without error
+// eg here <T extends string | number> generics very helpful
+const List = <T extends { id: number }>({ items, onClick }: ListProps<T>) => {
+  return (
+    <div>
+      <h2>List of items</h2>
+      {items.map((item, index) => (
+        <div key={item.id} onClick={() => onClick(item)}>
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default List;
