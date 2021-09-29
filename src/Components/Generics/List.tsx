05045be7c9bc760e0493bfe88c4eb633dcbab660
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-09-29 10:41:50 +0200

    fixed error

--- src/Components/Generics/List.tsx
+++ src/Components/Generics/List.tsx
@@ -11,11 +11,13 @@ const List = <T extends { id: number }>({ items, onClick }: ListProps<T>) => {
   return (
     <div>
       <h2>List of items</h2>
-      {items.map((item, index) => (
-        <div key={item.id} onClick={() => onClick(item)}>
-          {item}
-        </div>
-      ))}
+      {items.map((item) => {
+        return (
+          <div key={item.id} onClick={() => onClick(item)}>
+            {item.id}
+          </div>
+        );
+      })}
     </div>
   );
 };
