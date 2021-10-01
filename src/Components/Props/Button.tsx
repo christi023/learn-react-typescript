a91373f1bd42b5ff10188298a0cf016037b1dd5f
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-10-01 10:53:17 +0200

    clean up code

--- src/Components/Props/Button.tsx
+++ src/Components/Props/Button.tsx
@@ -1,16 +1,12 @@
-import React from "react";
+import React from 'react';
 
 // events and button with id parameter
 type ButtonProps = {
-  handleClick: (event: React.MouseEvent<HTMLButtonElement>, id:number) => void // do not return anything
-}
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>, id: number) => void; // do not return anything
+};
 
 const Button = (props: ButtonProps) => {
-  return (
-    <button onClick={(event) => props.handleClick(event, 1)}>
-      Click
-    </button>
-  );
+  return <button onClick={(event) => props.handleClick(event, 1)}>Click</button>;
 };
 
 export default Button;
